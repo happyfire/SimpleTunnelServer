c247c7959c1e349616129345e6845ba712dd83b8
wuhao <hao.wu@dreamingame.net>
2017-06-29 14:05:26 +0800

    tun0

--- forward.sh
+++ forward.sh
@@ -1,4 +1,4 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -A FORWARD -i tun1 -o eth0 -j ACCEPT
-iptables -A FORWARD -i eth0 -o tun1 -j ACCEPT
+iptables -A FORWARD -i tun0 -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth0 -o tun0 -j ACCEPT
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
