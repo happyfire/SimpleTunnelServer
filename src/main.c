1a09c797479fe7a304f5db871d7294ff10a73920
wuhao <hao.wu@dreamingame.net>
2017-07-06 16:36:55 +0800

    optional_argument

--- src/main.c
+++ src/main.c
@@ -7,10 +7,10 @@
 #include "tunnel_server.h"
 #include "utils.h"
 
-static const char *short_options = "hp:v:";
+static const char *short_options = "hp:v";
 static const struct option long_options[] = {
         {"port", required_argument, NULL, 'p'},
-        {"verbose", required_argument, NULL, 'v'},
+        {"verbose", optional_argument, NULL, 'v'},
         {"help", no_argument, NULL, 'h'},
         {NULL, 0, NULL, 0}
 };
@@ -18,20 +18,24 @@ static const struct option long_options[] = {
 int main(int argc, char *argv[])
 {
     char *port = "4000";
-    g_verbose = 0;
 
     int opt = 0;
     while( (opt = getopt_long(argc, argv, short_options, long_options, NULL)) != -1){
         switch (opt){
             case 'h':
             case '?':
-                fprintf(stdout, "Usage: %s --port|-p {port} --verbose|-v {verbose_level}\n", argv[0]);
+                fprintf(stdout, "Usage: %s [--port|-p {port}] [-v|--verbose|--v {=verbose_level}]\n", argv[0]);
                 return 0;
             case 'p':
                 port = optarg;
                 break;
             case 'v':
-                g_verbose = atoi(optarg);
+                if(optarg==NULL){
+                    g_verbose = 1;
+                }
+                else {
+                    g_verbose = atoi(optarg);
+                }
                 break;
         }
     }
